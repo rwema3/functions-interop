b4362bf2ecc56d2be82a37e153d77214b61fd3ca
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-02 18:51:52 -0700

    return futureToPromise(result);     }     // See: https://stackoverflow.com/questions/47128440/google-firebase-errorfunction-returned-undefined-expected-promise-or-value     return 0;

--- lib/firebase_functions_interop.dart
+++ lib/firebase_functions_interop.dart
@@ -344,3 +344,7 @@ class DocumentBuilder {
     final context = new EventContext(jsContext);
     var result = handler(snapshot, context);
     if (result is Future) {
+      return futureToPromise(result);
+    }
+    // See: https://stackoverflow.com/questions/47128440/google-firebase-errorfunction-returned-undefined-expected-promise-or-value
+    return 0;
